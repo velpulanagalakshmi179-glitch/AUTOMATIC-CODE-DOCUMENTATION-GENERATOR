5b9246bdfceb88dc07fc2e92085b52e6063507a8
Nagalakshmi <velpulanagalakshmi179@gmail.com>
2026-03-11 14:00:31 +0530

    Move config.js to frontend folder

---
+++ frontend/config.js
@@ -0,0 +1,3 @@
+// Set your Render backend URL here after deploying to Render
+// Replace the URL below with your actual Render URL
+window.BACKEND_URL = "https://automatic-code-documentation-generator.onrender.com/api";
